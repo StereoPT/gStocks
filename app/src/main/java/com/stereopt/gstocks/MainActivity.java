5bf09b3ff3f3e643634a058498fe61cf64ef04a7
GuidoSP <stereopt@gmail.com>
2019-03-27 10:32:21 +0000

    Search Activity Start

--- app/src/main/java/com/stereopt/gstocks/MainActivity.java
+++ app/src/main/java/com/stereopt/gstocks/MainActivity.java
@@ -1,6 +1,7 @@
 package com.stereopt.gstocks;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -55,6 +56,11 @@ public class MainActivity extends AppCompatActivity {
         }));
 
         prepareStockData();
+    }
+
+    public void btnAddNewStock(View view) {
+        Intent intent = new Intent(getApplicationContext(), SearchStock.class);
+        startActivity(intent);
     }
 
     private void prepareStockData() {
